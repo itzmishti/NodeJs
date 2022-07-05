f5ec02bfacc509cd4d6ddcc516b004a09891d7b3
itzmishti <vgupta4_be20@thapar.edu>
2022-07-05 06:17:03 +0530

    eventClass

---
+++ TutorialOnModules/app2.js
@@ -0,0 +1,31 @@
+const EventEmitter=require('events');
+const eventEmitter=new EventEmitter();
+
+eventEmitter.on('tutorial',(n1,n2)=>{
+  console.log(n1+n2);
+});
+
+eventEmitter.emit('tutorial',1,2);
+
+class Person extends EventEmitter{
+  constructor(name){
+    super();
+    this._name=name;
+  }
+  get name(){
+    return  this._name;
+  }
+
+}
+let pedro=new Person('Pedro');
+pedro.on('name',()=>{
+  console.log('my name is '+pedro.name);
+})
+pedro.emit('name');
+
+let chris=new Person('chris');
+chris.on('name',()=>
+{
+  console.log('my name is '+chris.name);
+})
+chris.emit('name');
