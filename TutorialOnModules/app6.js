69b800775610713aca17ee866adfd8e84036cbb2
itzmishti <vgupta4_be20@thapar.edu>
2022-07-05 15:34:36 +0530

    fileSystemModule2+zip

--- TutorialOnModules/app6.js
+++ TutorialOnModules/app6.js
@@ -1,7 +1,14 @@
 const fs=require('fs');
-const readSream=fs.createReadStream('./example.txt','utf8');
-const writeStream=fs.createWriteStream('example3.txt');
-readSream.on('data',(chunk)=>{
-  writeStream.write(chunk);
-  console.log(chunk);
-});
+const zlib=require('zlib');
+const gzip=zlib.createGzip();
+const gunzip=zlib.createGunzip();
+
+const readStream=fs.createReadStream('./example.txt','utf8');
+const writeStream=fs.createWriteStream('example62.txt.gz');
+//Way -1
+// readSream.on('data',(chunk)=>{
+//   writeStream.write(chunk);
+// });
+//Way 2:Using pipes
+readStream.pipe(gzip).pipe(writeStream);//compressing a file before writing
+//readStream.pipe(gunzip).pipe(writeStream);//for unzipping
