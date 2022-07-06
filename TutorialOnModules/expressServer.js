0eb80c063c58b11db9dd000a429c9b34431eece7
itzmishti <vgupta4_be20@thapar.edu>
2022-07-06 06:39:02 +0530

    express server

--- TutorialOnModules/expressServer.js
+++ TutorialOnModules/expressServer.js
@@ -1,7 +1,16 @@
 const express=require('express');
+const path=require('path');
 const app=express();
 
+app.use('/public',express.static(path.join(__dirname,'tutorial5')));
 app.get('/',(req,res)=>{
-  res.send('Hello World');
+  //res.send('Hello World');
+  res.send(path.join(__dirname,'tutorial5','a.txt'));
 });
 app.listen('3000');
+
+app.get('/:name/:age',(req,res)=>{
+  console.log(req.params);
+  res.send('example with route params');
+  res.send(req.params.name+" : "+req.params.age);
+});
